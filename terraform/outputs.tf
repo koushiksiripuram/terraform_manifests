b23ae179377a40b849fc5dab84fb0e11e5b9a50b
koushiksiripuram <koushiksiripuram48@gmail.com>
2026-05-22 15:16:39 +0530

    certbox

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,5 +8,5 @@ output "ec2_public_dns" {
   value = aws_instance.ghost_server.public_dns
 }
 output "elastic_ip" {
-  value = aws_eip.ghost_eip.public_ip
+  value = data.aws_eip.ghost_eip.public_ip
 }
